ff89eb612a7ced12b514d7daa3619c7f3a468b07
AnthonyZet <zejda.anthony@gmail.com>
2023-02-11 20:09:32 +0100

    working

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,36 +1,72 @@
-import React, { useState } from "react";
-import {addDoc, collection, serverTimestamp} from 'firebase/firestore';
-import {auth, db} from '../firebase-config';
+import React, { useState, useEffect } from "react";
+import { db, auth } from "../firebase-config";
+import {
+  collection,
+  addDoc,
+  where,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
 
 
-export const Chat = (props) => {
-    const {room} = props;
+export const Chat = ({ room }) => {
+  const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
-  const messagesRef = collection(db, 'messages')
+  const messagesRef = collection(db, "messages");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if(newMessage === "") return;
-   
+  useEffect(() => {
+    const queryMessages = query(
+      messagesRef,
+      where("room", "==", room),
+      orderBy("createdAt")
+    );
+    const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(messages);
+      setMessages(messages);
+    });
+
+    return () => unsuscribe();
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (newMessage === "") return;
     await addDoc(messagesRef, {
-        text: newMessage,
-        createdAt: serverTimestamp(),
-        user: auth.currentUser.displayName,
-        room,
-        });
-        setNewMessage("");
-    };
+      text: newMessage,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser.displayName,
+      room,
+    });
 
-       
+    setNewMessage("");
+  };
 
   return (
     <div className="chat-app">
+      <div className="header">
+        <h1>Welcome to: {room.toUpperCase()}</h1>
+      </div>
+      <div className="messages">
+        {messages.map((message) => (
+          <div key={message.id} className="message">
+            <span className="user">{message.user}:</span> {message.text}
+          </div>
+        ))}
+      </div>
       <form onSubmit={handleSubmit} className="new-message-form">
         <input
+          type="text"
+          value={newMessage}
+          onChange={(event) => setNewMessage(event.target.value)}
           className="new-message-input"
           placeholder="Type your message here..."
-          onChange={(e)=>setNewMessage(e.target.value)}
-          value={newMessage}
         />
         <button type="submit" className="send-button">
           Send
