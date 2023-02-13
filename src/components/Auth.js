33c41ddb161036d5a89986255d502496fc879046
AnthonyZet <zejda.anthony@gmail.com>
2023-02-13 18:41:29 +0100

    css

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -18,11 +18,12 @@ export const Auth = (props) => {
   };
   return (
     <section className="w-screen h-screen auth flex justify-center items-center">
-      <div className="flex justify-around items-center w-[800px] h-[430px] border-b-4 powerShell text-2xl">
+      <div className="flex justify-around items-center w-[800px] h-[430px] border-b-4 powerShell text-2xl relative">
       <p>C:\  </p>
       <p className="ml-4">λ <span className="anime">|</span></p>
       <p className="text-red">Přihlaš se Googlem:</p>
       <button onClick={signInWithGoogle} className=" px-4 mr-36 hover:opacity-0 ease-in-out duration-700 bg-red-700 animate-pulse">Přihlásit</button>
+      <p className="absolute top-full right-0 text-green-700">€nty</p>
       </div>
     </section>
   );
